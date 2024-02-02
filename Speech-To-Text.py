54bba32b1cc4c62c58dbb2a46f3ee9d8b44d8122
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2024-02-02 17:42:38 -0600

    Update Speech-To-Text.py

--- Speech-To-Text.py
+++ Speech-To-Text.py
@@ -3,7 +3,7 @@ import whisper
 model = whisper.load_model("base")
 
 # load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("audio.mp3")
+audio = whisper.load_audio("audio.avi")
 audio = whisper.pad_or_trim(audio)
 
 # make log-Mel spectrogram and move to the same device as the model
