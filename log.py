94ace7b3e91ccd07bda140a21ffd05d5a8dc8982
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2024-02-01 18:10:12 -0600

    Create log.py

---
+++ log.py
@@ -0,0 +1,35 @@
+from TikTokLive import TikTokLiveClient
+from TikTokLive.types.events import CommentEvent, ConnectEvent
+import os
+import logging
+import time
+
+# Set up logging
+time_stamp = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+log_file = f"logs/{time_stamp}.log"
+logging.basicConfig(filename=log_file, level=logging.DEBUG)
+
+
+# Instantiate the client with the user's username
+user = os.environ.get("TIKTOK_USER")
+client: TikTokLiveClient = TikTokLiveClient(unique_id=user)
+
+
+# Define how you want to handle specific events via decorator
+@client.on("connect")
+async def on_connect(_: ConnectEvent):
+    print("Connected to Room ID:", client.room_id)
+
+
+# Notice no decorator?
+async def on_comment(event: CommentEvent):
+    print(f"{event.user.nickname} -> {event.comment}")
+
+
+# Define handling an event via "callback"
+client.add_listener("comment", on_comment)
+
+if __name__ == '__main__':
+    # Run the client and block the main thread
+    # await client.start() to run non-blocking
+    client.run()
