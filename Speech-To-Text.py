e0b625b28d1db36bfed2402509d5df237b1fcf66
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2024-02-02 17:56:01 -0600

    Update Speech-To-Text.py

--- Speech-To-Text.py
+++ Speech-To-Text.py
@@ -1,9 +1,13 @@
 import whisper
+import ffmpeg
+
+#primero convertir de avi a mp3
+ffmpeg.input('audio.avi').output('audio.mp3').run()
 
 model = whisper.load_model("base")
 
 # load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("audio.avi")
+audio = whisper.load_audio("audio.mp3")
 audio = whisper.pad_or_trim(audio)
 
 # make log-Mel spectrogram and move to the same device as the model
