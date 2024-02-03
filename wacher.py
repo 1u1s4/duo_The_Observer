d9eb9d9010229bb63b567baa382eb0c54f052d72
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2024-02-02 19:13:15 -0600

    Update wacher.py

--- wacher.py
+++ wacher.py
@@ -1,5 +1,12 @@
 from TikTokLive import TikTokLiveClient
 from TikTokLive.types.events import CommentEvent, ConnectEvent
 
-client: TikTokLiveClient = TikTokLiveClient(unique_id="@gashi_137")
-print(client.room_id)
+
+def on_connect(unique_id: str) -> bool:
+    client: TikTokLiveClient = TikTokLiveClient(unique_id=unique_id)
+    respuesta = client.room_id
+
+    return bool(respuesta)
+
+if __name__ == '__main__':
+    print(on_connect("klg.gt"))
