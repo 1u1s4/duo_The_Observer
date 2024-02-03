d834e5a582b1fa6124e7970aab7f710d273fd8ec
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2024-02-02 19:24:52 -0600

    Update wacher.py

--- wacher.py
+++ wacher.py
@@ -3,12 +3,15 @@ from TikTokLive.types.events import CommentEvent, ConnectEvent
 
 
 def on_connect(unique_id: str) -> bool:
-    client: TikTokLiveClient = TikTokLiveClient(unique_id=unique_id)
-    client.run()
-    respuesta = client.room_id
-    print(respuesta)
+    try:
+        client: TikTokLiveClient = TikTokLiveClient(unique_id=unique_id)
+        client.run()
+        respuesta = client.room_id
 
-    return bool(respuesta)
+        return bool(respuesta)
+    except Exception as e:
+        print(e)
+        return False
 
 if __name__ == '__main__':
     print(on_connect("klg.gt"))
